1b204710e3a630fe42f8269355822fa78bef78c1
Pho Thin Maung <101423229+phothinmg@users.noreply.github.com>
2024-06-17 18:54:07 +0630

    edit

--- hono-icons/c001847.js
+++ hono-icons/c001847.js
@@ -2,14 +2,19 @@ function c001847() {
   const head = document.querySelector("head");
   const styleTag = document.createElement("style");
   styleTag.innerHTML = `
-  .c001847{
-            color: #585858;
+    .c001847{
+        color: rgb(37, 33, 33);
+      }
+      .c001847:hover{
+        color: darkgreen;
+        cursor: pointer;
+      }
+      @media (prefers-color-scheme: dark){
+        .c001847{
+          color: goldenrod;
         }
-        @media (prefers-color-scheme: dark){
-            .c001847{
-            color: #ababab;
-        }
-        }
+      }
+        
   
   `;
   head.appendChild(styleTag);
