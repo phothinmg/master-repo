460da82707f39f56f8824e52f2d944584ceb0dee
Pho Thin Maung <101423229+phothinmg@users.noreply.github.com>
2024-08-08 23:05:36 +0630

    Add files via upload

---
+++ honoblog/highlight-copy-button.js
@@ -0,0 +1,16 @@
+// deno-lint-ignore-file
+hljs.addPlugin(
+  new CopyButtonPlugin({
+    hook: (text, el) => {
+      let { replace, replacewith } = el.dataset;
+      if (replace && replacewith) {
+        return text.replace(replace, replacewith);
+      }
+      return text;
+    },
+    callback: (text, el) => {
+      console.log(text);
+    },
+  })
+);
+hljs.highlightAll();
